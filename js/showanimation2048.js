132c67ca6f4e0790854f7e62cc871ded8259c1a8
ooowQAQ <49637819+ooowQAQ@users.noreply.github.com>
2021-04-04 13:37:45 +0000

    添加分数增加动画

--- js/showanimation2048.js
+++ js/showanimation2048.js
@@ -20,6 +20,46 @@ function showMoveAnimation(fromx, fromy, tox, toy) {
     }, 200);
 }
 
-function updateScore(score) {
-    $("#score").text(score);
+
+// 记录分数，分数增加，
+function addScore(score) {
+	var span = document.getElementsByClassName("number"),
+		currentScore = parseInt(span[0].innerText),
+		bestScore = parseInt(span[1].innerText);
+	span[0].innerText = score + currentScore;
+	scoreUpAnimaton("score", score);
+	if (span[0].innerText > bestScore) {
+		scoreUpAnimaton("best", score);
+		span[1].innerText = span[0].innerText;
+	}
 }
+
+// 分数增加 动画
+function scoreUpAnimaton(type, score) {
+	var ele,
+		score,
+		timer,
+		count = 0;
+	if (type == "score") {
+		ele = document.getElementsByClassName("score-animation")[0];
+	} else if (type == "best") {
+		ele = document.getElementsByClassName("best-animation")[0];
+	}
+	score = "+" + score;
+	ele.innerText = score;
+	ele.style.top = "25px";
+	ele.style.color = "#8f7a66";
+	ele.style.opacity = "1.0"
+
+	timer = setInterval(function() {
+		count ++;
+		ele.style.display = "inline-block";
+		ele.style.top = parseInt(ele.style.top) - 8 + "px";
+		ele.style.opacity = parseFloat(ele.style.opacity) - 0.1;
+		if (count == 6) {
+			clearInterval(timer);
+			ele.style.display = "none";
+		}
+	}, 80);
+}
+
